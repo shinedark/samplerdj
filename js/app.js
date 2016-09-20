700f67899157452c727215366f0b25e0f566c016
shinedark <mide0303@gmail.com>
2016-09-20 12:36:50 -0500

    2nd try

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ var KeyMapper = {
 	}
 }
 
-KeyMapper.create(32, 'SPACE');
 KeyMapper.create(81, 'Q');
 KeyMapper.create(87, 'W');
 KeyMapper.create(69, 'E');
@@ -26,9 +25,23 @@ KeyMapper.create(85, 'U');
 KeyMapper.create(73, 'I');
 KeyMapper.create(79, 'O');
 KeyMapper.create(80, 'P');
+KeyMapper.create(65, 'A');
+KeyMapper.create(83, 'S');
+KeyMapper.create(68, 'D');
+KeyMapper.create(70, 'F');
+KeyMapper.create(71, 'G');
+KeyMapper.create(72, 'H');
+KeyMapper.create(74, 'J');
+KeyMapper.create(75, 'K');
+KeyMapper.create(76, 'L');
+KeyMapper.create(90, 'Z', 'Delay2');
 KeyMapper.create(88, 'X', 'Delay');
 KeyMapper.create(67, 'C', 'Filter');
 KeyMapper.create(86, 'V', 'Reverb');
+KeyMapper.create(66, 'B', 'PingDelay');
+KeyMapper.create(78, 'N', 'Flanger');
+KeyMapper.create(77, 'M', 'Hfilter');
+KeyMapper.create(32, 'SPACE');
 
 
 $(document).ready(function() {
@@ -95,7 +108,13 @@ function createSampler(audioSrc, keyCode, name) {
 		  this.isLooping = true;
 		},
 		render: function(){
-			$('.artwork').append('<li>Press ' + KeyMapper.getValue(this.keyCode) + ' for ' + this.name + '</li>');
+			// $('.artwork').append('<li>Press ' + KeyMapper.getValue(this.keyCode) + ' for ' + this.name + '</li>');
+			var node = $('<li>Press ' + KeyMapper.getValue(this.keyCode) + ' for ' + this.name + '</li>').appendTo('.artwork');
+			node.on('touchstart', function(evt){
+				this.play();
+			}).on('touchend', function(evt) {
+				this.stop();
+			});
 		},
 		hasFx: function(fx){
 			return this._aud.effects.includes(fx);
@@ -130,19 +149,65 @@ function createSampler(audioSrc, keyCode, name) {
 				sampler._aud.addEffect(reverb);
 			}		
 		}
+		else if (evt.keyCode == KeyMapper.getCode('Delay2') && sampler.isPlaying){
+			if (!sampler.hasFx(delay)){
+				sampler._aud.addEffect(delay);
+			}		
+		}
+		else if (evt.keyCode == KeyMapper.getCode('PingDelay') && sampler.isPlaying){
+			if (!sampler.hasFx(pingPongDelay)){
+				sampler._aud.addEffect(pingPongDelay);
+			}		
+		}
+		else if (evt.keyCode == KeyMapper.getCode('Flanger') && sampler.isPlaying){
+			if (!sampler.hasFx(flanger)){
+				sampler._aud.addEffect(flanger);
+			}		
+		}
+		else if (evt.keyCode == KeyMapper.getCode('Hfilter') && sampler.isPlaying){
+			if (!sampler.hasFx(highPassFilter)){
+				sampler._aud.addEffect(highPassFilter);
+			}		
+		}
 
 	}).keyup(function(evt) {
 		if (evt.keyCode == sampler.keyCode && !sampler.isLooping) {
 			sampler.stop();
 		}
 		else  if (evt.keyCode == KeyMapper.getCode('Delay') ) {
-			sampler._aud.removeEffect(dubDelay);
+			if (sampler.hasFx(dubDelay)){
+				sampler._aud.removeEffect(dubDelay);
+			}
 		}
 		else  if (evt.keyCode == KeyMapper.getCode('Filter') ) {
-			sampler._aud.removeEffect(lowPassFilter); 	
+			if (sampler.hasFx(lowPassFilter)){
+				sampler._aud.removeEffect(lowPassFilter);
+			}	
 		}
 		else  if (evt.keyCode == KeyMapper.getCode('Reverb') ) {
-			sampler._aud.removeEffect(reverb);
+			if (sampler.hasFx(reverb)){
+				sampler._aud.removeEffect(reverb);
+			}
+		}
+		else  if (evt.keyCode == KeyMapper.getCode('Delay2') ) {
+			if (sampler.hasFx(delay)){
+				sampler._aud.removeEffect(delay);
+			}
+		}
+		else  if (evt.keyCode == KeyMapper.getCode('PingDelay') ) {
+			if (sampler.hasFx(pingPongDelay)){
+				sampler._aud.removeEffect(pingPongDelay);
+			}
+		}
+		else  if (evt.keyCode == KeyMapper.getCode('Flanger') ) {
+			if (sampler.hasFx(flanger)){
+				sampler._aud.removeEffect(flanger);
+			}
+		}
+		else  if (evt.keyCode == KeyMapper.getCode('Hfilter') ) {
+			if (sampler.hasFx(highPassFilter)){
+				sampler._aud.removeEffect(highPassFilter);
+			}
 		}
 
 	});
@@ -173,8 +238,42 @@ var reverb = new Pizzicato.Effects.Reverb({
     time: 1,
     decay: 0.8,
     reverse: true,
-    mix: 0.75
+    mix: 0.5
 });
+
+var delay = new Pizzicato.Effects.Delay({
+    feedback: 0.3,
+    time: 0.3,
+    mix: 0.5
+});
+
+var flanger = new Pizzicato.Effects.Flanger({
+    time: 0.45,
+    speed: 0.5,
+    depth: 0.3,
+    feedback: 0.1,
+    mix: 1
+});
+
+var highPassFilter = new Pizzicato.Effects.HighPassFilter({
+    frequency: 1200,
+    peak: 10
+
+});
+
+var pingPongDelay = new Pizzicato.Effects.PingPongDelay({
+    feedback: 0.3,
+    time: 0.2,
+    mix: 0.68
+});
+
+
+
+
+
+
+
+
 
 
 // $(document).ajaxStart(function(){
@@ -182,15 +281,15 @@ var reverb = new Pizzicato.Effects.Reverb({
 // }
 
 
-// create jason for itunes  figure how to promp 10 songs into the 10 keys and display artwork let them search by genre
+// create jason for itunes  figure how to promp songs into the keys and display artwork let them search by genre
+// droppable function in jquery might work for drag and drop songs 
+// create a function that creates the  samplers 
  
-// create a function that creates the 10 samplers 
-// add controling parameters to fx when key press 
-// add fx to display 
+ 
 
 
 
-//limit to top 10 rated songs 
+//limit to top 10 rated songs by artist 
 
 
 
